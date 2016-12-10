3cc2bbc7667336ac89136dcded32b4ead4e144ba
i5ting <i5ting@126.com>
2016-12-10 22:55:25 +0800

    document, please ignore

--- viewctrl.js
+++ viewctrl.js
@@ -8,7 +8,7 @@ module.exports = class MyViewController extends ViewController {
   }
   
   get() { 
-    var a = this.query.a
+    let a = this.query.a
     // this.renderType='view'
     return {
       tpl: 'index',
